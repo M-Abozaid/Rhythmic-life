1c8dd411a878e16a30e3e5b8d711618e80adb9d3
M-Abozaid <aapozaid@gmail.com>
2017-03-18 11:03:16 +0200

    adding files config/

---
+++ config/config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+    shared: {
+    },
+ 
+    development: {
+    	redis: {
+            port: 3844,
+            host: '50.30.35.9',
+            pass: '5e6b16047caeb75191d9c0cb77bbb9a6',
+            db: 16
+        },
+        db: 'mongodb://Muhammed:012580504999@users-shard-00-00-gkxxn.mongodb.net:27017,users-shard-00-01-gkxxn.mongodb.net:27017,users-shard-00-02-gkxxn.mongodb.net:27017/users?ssl=true&replicaSet=users-shard-0&authSource=admin',
+                     
+    	fbPageToken: 'EAAGCwOXSeYsBAII8O2PpPLM2kVNv80Ge78FR1tuima9fJdzTI5RGufCqBmqN4aICPMN9ZCENFhomY3EOZBdqZBZAVOFzYwvvw2ZA2GanvVVNOPcxAbBAO3HR5ztoV9z2L4hZBPkjUIYKkxTdZC6DgFvcGJZAL28Ftxrha60fge5pOwZDZD',
+    	fbPageID: '770789176412771',
+    	fbWebhookVerifyToken: 'Hello-dude',
+    	witToken: 'UUFARUEEEUYJJHUZSYO3JGMPPLKVBOV5'
+    },
+ 
+    production: {
+
+    }
+}
+
+//770789176412771
