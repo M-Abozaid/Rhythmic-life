c5335f86cf8243eb3facd245b4dc4fa239be063a
M-Abozaid <aapozaid@gmail.com>
2017-03-21 11:00:36 +0200

    fix

--- actions/sendQuickReply.js
+++ actions/sendQuickReply.js
@@ -15,23 +15,23 @@ module.exports = function({sessionId, context, text, entities}) {
 		debug(`Session ${sessionId} received ${text}`);
 		debug(`The current context is ${JSON.stringify(context)}`);
 		debug(`Wit extracted ${JSON.stringify(entities)}`);
-		console.log('user'+context.userData.first_name);
+		console.log('user '+context.userData.first_name);
 		console.log('entities '+ JSON.stringify(entities));
-		/*
-		const diary = helper.getEntityValues(entities ,'diary' );
+		
+		//const diary = helper.getEntityValues(entities ,'diary' );
 		//let addActivity = entities.diary[0].value;
-		console.log('entitiy value '+diary);
+		//console.log('entitiy value '+diary);
 		//const replies = [];
 		//if(diary){
-		const replies = ['habit','occasional'];
+		var replies = {0:'habit',1:'occasional'};
 		const text1 = 'allright! tell me the name of the activity?';
 		//}
 
 		let data = platformHelpers.generateQuickReplies(text1, replies);
 		
 		return GraphAPI.sendTemplateMessage(recipientId, data);
-		*/
-		return GraphAPI.sendPlainMessage(recipientId, 'Good evening '+context.userData.first_name);
+		
+		//return GraphAPI.sendPlainMessage(recipientId, 'Good evening '+context.userData.first_name);
 
 		
 	})
