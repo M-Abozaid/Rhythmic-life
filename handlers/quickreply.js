e24c733bebb423dba2047f682a81d87924d38952
M-Abozaid <aapozaid@gmail.com>
2017-03-21 13:12:12 +0200

    try

--- handlers/quickreply.js
+++ handlers/quickreply.js
@@ -5,6 +5,23 @@ const platformHelpers = require('../platformHelpers');
 
 
 module.exports = function handleQuickReply(sender, sessionId, context, payload) {
+		context.message = msg;
+	console.log('inside handleTextMessage')
+	wit.runActions(sessionId, msg, context, (error, context) => {
+		if (error) {
+			console.log('Oops! Got an error from Wit:', error);
+			return;
+		} 
+		
+		console.log('Waiting for futher messages.');
+
+		if (context['done']) {
+			console.log('context done inside runActions');
+		   	sessionStore.destroy(sessionId);
+		}			
+	});
+
+	/*
 	let payloadTokens = payload.split(':');
 	const action = payloadTokens[0];
 	const data = payloadTokens[1];
@@ -21,6 +38,7 @@ module.exports = function handleQuickReply(sender, sessionId, context, payload) 
 			return sendGenericCardsSample(sender);
 			break;
 	}
+	*/
 }
 
 
