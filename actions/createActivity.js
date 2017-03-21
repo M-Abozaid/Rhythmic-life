7a5f6b44394b2b0d81a354b300d87b560569006e
M-Abozaid <aapozaid@gmail.com>
2017-03-21 11:26:59 +0200

    fix

--- actions/createActivity.js
+++ actions/createActivity.js
@@ -3,10 +3,12 @@
 const GraphAPI = require('../graphAPI');
 const sessionStore = require('../sessionStore');
 const debug = require('debug')('cbp:actions:sample');
+const helper = require('../witHelpers');
 
 
 module.exports = function({sessionId, context, text, entities}) {
-	console.log('context '+JSON.stringify(context));
+	console.log('context in create'+JSON.stringify(context));
+	console.log('entities in create '+ JSON.stringify(entities));
 	return sessionStore.get(sessionId)
 	.then(session => {
 		const recipientId = session.fbid;
@@ -15,9 +17,10 @@ module.exports = function({sessionId, context, text, entities}) {
 		debug(`Wit extracted ${JSON.stringify(entities)}`);
 		console.log('user'+context.userData.first_name);
 		console.log('entities '+ JSON.stringify(entities));
-		let activityName = entities.activity_name[0].value;
-		let activityType = entities.activity_type[0].value;
-
+		//let activityName = entities.activity_name[0].value;
+		//let activityType = entities.activity_type[0].value;
+		let activityName = helper.getEntityValues(entities ,'acitvity_name' );
+		let activityType = helper.getEntityValues(entities ,'activity_type' );
 		console.log('entitiy1 value '+activityName+' entitiy2 value '+activityType);
 
 		context.message = 'created activity '+activityName ;
