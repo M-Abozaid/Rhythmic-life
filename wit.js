64194ac4b588dc529bd36da3658da38d5865cde6
M-Abozaid <aapozaid@gmail.com>
2017-03-22 07:07:24 +0200

    adding action select-joke

--- wit.js
+++ wit.js
@@ -16,54 +16,99 @@ const config = require('./config');
 // bot actions
 const actions = {
 	send(request, response) {
-
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-        console.log('user said...', request.text);
-        console.log('sending...', JSON.stringify(response));
-        return resolve(context);
-    });		
-
 		/*
-		const {sessionId, context, entities} = request;
-   		const {text, quickreplies} = response;
+		const {
+			sessionId,
+			context,
+			entities
+		} = request;
+		const {
+			text,
+			quickreplies
+		} = response;
+		return new Promise(function(resolve, reject) {
+			console.log('user said...', request.text);
+			console.log('sending...', JSON.stringify(response));
+
+			return resolve();
+		});
+		*/
+
+		const {
+			sessionId,
+			context,
+			entities
+		} = request;
+		const {
+			text,
+			quickreplies
+		} = response;
 		debug('saying', response);
 
 		let recipientId;
 		return sessionStore.get(request.sessionId)
-		.then(session => {
-			console.log('user said...', request.text);
-      		console.log('sending...', JSON.stringify(response));
-			recipientId = session.fbid;
-			return GraphAPI.sendTemplateMessage(recipientId, response);
-		}).then(function() {
-			return context;
-		})
-		.catch((err) => {
-			console.log('Oops! An error occurred while forwarding the response to', recipientId, ':', err );
+			.then(session => {
+				console.log('user said...', request.text);
+				console.log('sending...', JSON.stringify(response));
+				recipientId = session.fbid;
+				if (quickreplies) {
+					var replies = {
+						0: 'habit',
+						1: 'occasional'
+					};
+					const text1 = 'Choose the activity type';
+
+					let response = platformHelpers.generateQuickReplies(text1, replies);
+				}
+				return GraphAPI.sendTemplateMessage(recipientId, response);
+			}) //.then(function() {
+			//return context;
+			//})
+			.catch((err) => {
+				console.log('Oops! An error occurred while forwarding the response to', recipientId, ':', err);
+			});
+
+	},
+
+	select-joke ({sessionId, context, text, entities}) {
+	console.log('context '+JSON.stringify(context));
+	return sessionStore.get(sessionId)
+	.then(
+		return new Promise(function(resolve, reject) {
+			// const category = firstEntityValue(entities, 'category') || 'default';
+			// const sentiment = firstEntityValue(entities, 'sentiment');
+			// if (sentiment) {
+			//   context.ack = sentiment === 'positive' ? 'Glad you liked it.' : 'Hmm.';
+			// } else {
+			//   delete context.ack;
+			// }
+
+			//const jokes = allJokes[context.cat || 'default'];
+			//context.joke = jokes[Math.floor(Math.random() * jokes.length)];
+			return resolve(context);
 		});
-		*/
-    }
+	)}
+
 
 };
 
 
 
 (function initCustomActions() {
-    var actionsPath = path.join(__dirname, 'actions');
-    var actionsFile = fs.readdirSync(actionsPath);
+	var actionsPath = path.join(__dirname, 'actions');
+	var actionsFile = fs.readdirSync(actionsPath);
 
-    actionsFile.forEach(function (js) {
-    	const actionName = path.basename(js,'.js');
-        debug(`Initializing wit action [${actionName}]`);
-        actions[actionName] = require(path.join(actionsPath, js));
-    });
+	actionsFile.forEach(function(js) {
+		const actionName = path.basename(js, '.js');
+		debug(`Initializing wit action [${actionName}]`);
+		actions[actionName] = require(path.join(actionsPath, js));
+	});
 })();
 
 
 
-
-
 // Setting up our bot
-module.exports = new Wit({accessToken: config.witToken, actions: actions});
+module.exports = new Wit({
+	accessToken: config.witToken,
+	actions: actions
+});
