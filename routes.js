7916e8ef523fb134993c538379281a1a4fb28fd5
M-Abozaid <aapozaid@gmail.com>
2017-04-07 17:20:20 +0200

    change back

--- routes.js
+++ routes.js
@@ -51,6 +51,7 @@ exports.receive = function(req, res, next) {
 	res.status(200).end();
 	console.log("A POST REQUEST RECEIVED" );
 	const messages = extractMessagingObjects(req.body);
+	if(!(messages[0].sender.id)){next();}
 	if(messages[0].sender.id==FB_PAGE_ID){next();}
 	if(messages[0].sender.id==FB_PAGE_ID){return;}
 	if (!messages.length) {
