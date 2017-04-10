a6b80d63616305c6ede3bb2db9c4089487faf270
M-Abozaid <aapozaid@gmail.com>
2017-04-10 04:47:30 +0200

    try

--- takeAction.js
+++ takeAction.js
@@ -1,18 +1,21 @@
 const addActivity = require('./actions/addActivity')
 
 let takeAction = function(context,msg){
+	
+if(msg == 'hi'){
+	context.first.main = {};
+	let data = platformHelpers.generateQuickReplies('Would you like to add new activity', {0:'yes',1:'no'});
+	GraphAPI.sendPlainMessage(recipientId, 'Hello! ').then(  //+context.userData.first_name).then()
+	()=>{GraphAPI.sendTemplateMessage(recipientId, data)})
+}
+
 if (Object.keys(context.first.main).length == 0){ // if No context 
 
 	if (msg == "add activity"){
 		addActivity(context,msg);
 	}
 
-	if(msg == 'hi'){
-		context.first.main = {};
-		let data = platformHelpers.generateQuickReplies('Would you like to add new activity', {0:'yes',1:'no'});
-		GraphAPI.sendPlainMessage(recipientId, 'Hello! ').then(  //+context.userData.first_name).then()
-		()=>{GraphAPI.sendTemplateMessage(recipientId, data)})
-	}
+	
 }else {
 
 }
