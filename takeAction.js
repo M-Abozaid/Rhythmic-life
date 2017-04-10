7bf3f5bd64ec350066d99be5d268ce68a96969f9
M-Abozaid <aapozaid@gmail.com>
2017-04-10 04:55:37 +0200

    fix

--- takeAction.js
+++ takeAction.js
@@ -3,27 +3,27 @@ const platformHelpers = require('./platformHelpers');
 const GraphAPI = require('./graphAPI');
 
 let takeAction = function(context,msg){
-
-if(msg == 'hi'){
-	context.first.main = {};
-	let data = platformHelpers.generateQuickReplies('Would you like to add new activity', {0:'yes',1:'no'});
-	GraphAPI.sendPlainMessage(recipientId, 'Hello! ').then(  //+context.userData.first_name).then()
-	()=>{GraphAPI.sendTemplateMessage(recipientId, data)})
-}
-
-if (Object.keys(context.first.main).length == 0){ // if No context 
-
-	if (msg == "add activity"){
-		addActivity(context,msg);
+	let recipientId = context.userData.recipientId;
+	if(msg == 'hi'){
+		context.first.main = {};
+		let data = platformHelpers.generateQuickReplies('Would you like to add new activity', {0:'yes',1:'no'});
+		GraphAPI.sendPlainMessage(recipientId, 'Hello! ').then(  //+context.userData.first_name).then()
+		()=>{GraphAPI.sendTemplateMessage(recipientId, data)})
 	}
 
-	
-}else {
+	if (Object.keys(context.first.main).length == 0){ // if No context 
 
-}
-return new Promise(function(resolve, reject){
-resolve(context)
-})
+		if (msg == "add activity"){
+			addActivity(context,msg);
+		}
+
+		
+	}else {
+
+	}
+	return new Promise(function(resolve, reject){
+	resolve(context)
+	})
 }
 
 module.exports = takeAction;
