df49a1b112bd1c5cd23532afa93c3714761e5b1a
M-Abozaid <aapozaid@gmail.com>
2017-04-10 02:04:37 +0200

    learning

--- actions/addActivity.js
+++ actions/addActivity.js
@@ -5,4 +5,8 @@ const GraphAPI = require('../graphAPI');
 module.exports = function(context, msg){
 
 	console.log('done');
+
+	context.first.main.addingActivity = true
+
+	console.log('context in addActivity ', JSON.stringify(context));
 }
