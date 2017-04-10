79c819b6339d73e61dd27fa572dba006c1f1fad9
M-Abozaid <aapozaid@gmail.com>
2017-04-11 00:22:08 +0200

    fixed yad

--- actions/addActivity.js
+++ actions/addActivity.js
@@ -19,7 +19,7 @@ module.exports = function(context, msg){
 				positivity: context.current.positivity,
 				hebitual: context.current.hebitual
 			})
-			console.log('Pushed ');
+			console.log('Pushed ', JSON.stringify(user),' user.activities ',JSON.stringify(user.activities));
 			user.save(function (err, user) {
                         if (err) throw err;
                         //res.json(user);
