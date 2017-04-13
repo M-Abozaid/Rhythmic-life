ab8256ed2e460e95ab3643e790ce8b67f6c71e85
M-Abozaid <aapozaid@gmail.com>
2017-04-13 11:13:21 +0200

    fix

--- routes/showRoute.js
+++ routes/showRoute.js
@@ -8,14 +8,15 @@ const User = require('../schemas/user');
 let showRouter = express.Router();
 showRouter.use(bodyParser.json())
 
-let idi = req.params.id
+
 
 showRouter.get('/:id',  function(req, res, next) {
+	let recipientId = req.params.id
 	User.findOne({recipientId : recipientId},(err,user)=>{
-				if (err) throw err;
+		if (err) throw err;
 
-				res.render('show', { userId: user });
-				console.log("id ",idi)
+		res.render('show', { userId: user });
+		console.log("id ",recipientId)
 
 	})
 	
