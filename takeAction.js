77aed0155d317303bd42c228cf22fe41bb20d15d
M-Abozaid <aapozaid@gmail.com>
2017-04-13 05:39:12 +0200

    changing link

--- takeAction.js
+++ takeAction.js
@@ -23,7 +23,7 @@ let takeAction = function(context,msg){
 				"buttons":[
 			      {
 			        "type":"web_url",
-			        "url":"https://mabozaid.tk",
+			        "url":"https://http://m-abozaid.github.io/home/",
 			        "title":"View Item",
 			        "webview_height_ratio": "compact"
 			      }
