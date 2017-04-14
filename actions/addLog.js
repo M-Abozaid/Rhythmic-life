00bbd494b15dac6e0c515aef67076e92ac72095a
M-Abozaid <aapozaid@gmail.com>
2017-04-14 03:47:58 +0200

    fenomenoal logic

--- actions/addLog.js
+++ actions/addLog.js
@@ -47,13 +47,28 @@ let recipientId = context.userData.recipientId; // here because it was not acces
 				list = _.map(user.activities,(elem)=>{return elem.name})
 				console.log('list first  ', JSON.stringify(list))
 				list.push('New activity')
+				let numOfQuick = list.length 
+				if(numOfQuick>11){
+					let numOfVeiws = Math.floor(numOfVeiws/10) 
+					context.current.thisVeiw = context.current.thisVeiw || 0
+					let view = list.splice(context.current.thisVeiw * 10 ,10)
+					view.push("See more!")
+					let data = platformHelpers.generateQuickReplies('Choose the activity ', view);
+					GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
+						if(context.current.thisVeiw!=numOfVeiws){context.current.thisVeiw += 1}else{context.current.thisVeiw = 0}
+						context.current.chooseLog = true;
+						resolve(context)
+					})
+				}else{
+					let data = platformHelpers.generateQuickReplies('Choose the activity ', list);
+					//platformHelpers.generateButtonsTemplate('Choose the activity ',[{butn:'option1',},{butn:'opti2'}])
+					GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
+						context.current.chooseLog = true;
+						resolve(context)
+					})
+				}
 				console.log('list sec ', JSON.stringify(list));
-				let data = platformHelpers.generateQuickReplies('Choose the activity ', list);
-				//platformHelpers.generateButtonsTemplate('Choose the activity ',[{butn:'option1',},{butn:'opti2'}])
-				GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
-					context.current.chooseLog = true;
-					resolve(context)
-				})
+				
 
 			})
 		}else{
@@ -65,11 +80,17 @@ let recipientId = context.userData.recipientId; // here because it was not acces
 					context.current.continue = true
 					resolve(context);
 				}else{
-					let data = platformHelpers.generateQuickReplies('Type a note to be included if you like.', ['No thats it']);
-					GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
-						context.current.logName = context.msg;
+					if(context.msg == "See more!"){
+						context.current.chooseLog = false
+						context.current.continue = true;
 						resolve(context)
-					})
+					}else{
+						let data = platformHelpers.generateQuickReplies('Type a note to be included if you like.', ['No thats it']);
+						GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
+							context.current.logName = context.msg;
+							resolve(context)
+						})
+					}
 				}
 			}else{
 				if(context.current.logName && !context.current.note){
