f032a9ae78ee0467bfb0b703fb4dbe314509858f
M-Abozaid <aapozaid@gmail.com>
2017-04-15 03:11:58 +0200

    try

--- public/service.js
+++ public/service.js
@@ -4,7 +4,7 @@ angular.module('MyBot')
 .constant("baseURL", "https://salty-plains-47076.herokuapp.com/")
 .factory('LogsFctory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
-        return $resource(baseURL + "show/logs/"+cUser.recipientId, null, {
+        return $resource(baseURL + "show/logs/"+ document.cUser.recipientId, null, {
             'update': {
                 method: 'PUT'
             }
