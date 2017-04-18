4e30b2867b82ce599946b2e12e01119e18482a0d
M-Abozaid <aapozaid@gmail.com>
2017-04-18 13:26:48 +0200

    I\'m phenomenal impleminting angular with the new design 4 try

--- public/controller.js
+++ public/controller.js
@@ -8,11 +8,12 @@ $scope.message = 'Loading...';
 $scope.showMenu = false;
 
 LogsFactory.query(
+
     function (response) {
         $scope.logs = response;
         let days = [];
 
-        for (var i = 0; i <= moment.duration(log[logs.length -1].time - logs[0].time).days() - 1; i++) {
+        for (var i = 0; i <= moment.duration(logs[logs.length -1].time - logs[0].time).days() - 1; i++) {
             days.push(moment(logs[0].time).add(i, 'days'));
         }
 
