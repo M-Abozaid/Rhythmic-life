5cbc445d94c2e3e34e8bdaaf6bc9e84a03b5b28e
M-Abozaid <aapozaid@gmail.com>
2017-04-19 01:45:07 +0200

    fixing the date issue with the day menue

--- services/redis.js
+++ services/redis.js
@@ -49,7 +49,6 @@ function createClient(config, returnBuffers) {
     });
 
     client.on('connect', function() {
-        console.log('redis is connected');
         redisIsReady = true;
     });
 
