2083bc90cdd59e7f1cc0f61687824cab91901a4d
M-Abozaid <aapozaid@gmail.com>
2017-04-19 10:47:27 +0200

    using google charts

--- public/controller.js
+++ public/controller.js
@@ -77,7 +77,11 @@ LogsFactory.query(
         return log.activity.type == "work";
         console.log('filt');
     });
-    
+
+    $scope.work.map(function(elem){
+        return [new Date(elem.time) , elem.span / (1000*60)]
+    })
+
      google.charts.load('current', {'packages':['corechart']});
 
       // Set a callback to run when the Google Visualization API is loaded.
@@ -90,23 +94,18 @@ LogsFactory.query(
 
         // Create the data table.
         var data = new google.visualization.DataTable();
-        data.addColumn('string', 'Topping');
-        data.addColumn('number', 'Slices');
-        data.addRows([
-          ['Mushrooms', 3],
-          ['Onions', 1],
-          ['Olives', 1],
-          ['Zucchini', 1],
-          ['Pepperoni', 2]
-        ]);
+        data.addColumn('datetime', 'time');
+        data.addColumn('number', 'time spent (min)');
+        data.addRows($scope.work);
 
         // Set chart options
-        var options = {'title':'How Much Pizza I Ate Last Night',
-                       'width':400,
-                       'height':300};
+        var options = {'title':'Work',
+                       curveType: 'function',
+                       legend: { position: 'bottom' }
+                   };
 
         // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
         chart.draw(data, options);
       }
 
