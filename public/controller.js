d7b1a5ee9adb32a3c71dc268d0bb0a1c8bb1e851
M-Abozaid <aapozaid@gmail.com>
2017-04-19 10:19:30 +0200

    using google charts

--- public/controller.js
+++ public/controller.js
@@ -77,53 +77,85 @@ LogsFactory.query(
         return log.activity.type == "work";
         console.log('filt');
     });
+    
+     google.charts.load('current', {'packages':['corechart']});
 
-    $scope.mdata = {}
-    $scope.mdata.dataset1 = $scope.work
-    console.log(" work ",$scope.work);
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.charts.setOnLoadCallback(drawChart);
 
-     $scope.data = {
-        dataset0: [
-          {x: 0, val_0: 0, val_1: 0, val_2: 0, val_3: 0},
-          {x: 1, val_0: 0.993, val_1: 3.894, val_2: 8.47, val_3: 14.347},
-          {x: 2, val_0: 1.947, val_1: 7.174, val_2: 13.981, val_3: 19.991},
-          {x: 3, val_0: 2.823, val_1: 9.32, val_2: 14.608, val_3: 13.509},
-          {x: 4, val_0: 3.587, val_1: 9.996, val_2: 10.132, val_3: -1.167},
-          {x: 5, val_0: 4.207, val_1: 9.093, val_2: 2.117, val_3: -15.136},
-          {x: 6, val_0: 4.66, val_1: 6.755, val_2: -6.638, val_3: -19.923},
-          {x: 7, val_0: 4.927, val_1: 3.35, val_2: -13.074, val_3: -12.625}
-        ]
-      };
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
 
-    $scope.options = {
-      series: [
-        {
-          axis: "y",
-          dataset: "dataset0",
-          key: "val_1",
-          label: "An area series",
-          color: "#1f77b4",
-          type: ['line', 'dot', 'area'],
-          id: 'mySeries0'
-        }
-      ],
-      axes: {x: {key: "x"}}
-    };
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
 
-    $scope.moptions = {
-        series: [
-        {
-          axis: "y",
-          dataset: "dataset1",
-          key: "span",
-          label: "An area series",
-          color: "#1f77b4",
-          type: ['line', 'dot', 'area'],
-          id: 'mySeries1'
-        }
-      ],
-      axes: {x: {key: "time"}}
-    }
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+    // $scope.mdata = {}
+    // $scope.mdata.dataset1 = $scope.work
+    // console.log(" work ",$scope.work);
+
+    //  $scope.data = {
+    //     dataset0: [
+    //       {x: 0, val_0: 0, val_1: 0, val_2: 0, val_3: 0},
+    //       {x: 1, val_0: 0.993, val_1: 3.894, val_2: 8.47, val_3: 14.347},
+    //       {x: 2, val_0: 1.947, val_1: 7.174, val_2: 13.981, val_3: 19.991},
+    //       {x: 3, val_0: 2.823, val_1: 9.32, val_2: 14.608, val_3: 13.509},
+    //       {x: 4, val_0: 3.587, val_1: 9.996, val_2: 10.132, val_3: -1.167},
+    //       {x: 5, val_0: 4.207, val_1: 9.093, val_2: 2.117, val_3: -15.136},
+    //       {x: 6, val_0: 4.66, val_1: 6.755, val_2: -6.638, val_3: -19.923},
+    //       {x: 7, val_0: 4.927, val_1: 3.35, val_2: -13.074, val_3: -12.625}
+    //     ]
+    //   };
+
+    // $scope.options = {
+    //   series: [
+    //     {
+    //       axis: "y",
+    //       dataset: "dataset0",
+    //       key: "val_1",
+    //       label: "An area series",
+    //       color: "#1f77b4",
+    //       type: ['line', 'dot', 'area'],
+    //       id: 'mySeries0'
+    //     }
+    //   ],
+    //   axes: {x: {key: "x"}}
+    // };
+
+    // $scope.moptions = {
+    //     series: [
+    //     {
+    //       axis: "y",
+    //       dataset: "dataset1",
+    //       key: "span",
+    //       label: "An area series",
+    //       color: "#1f77b4",
+    //       type: ['line', 'dot', 'area'],
+    //       id: 'mySeries1'
+    //     }
+    //   ],
+    //   axes: {x: {key: "time"}}
+    // }
 
     },
     function (response) {
