e310565bab42947ba54d036c8761c7a5e07b8b72
M-Abozaid <aapozaid@gmail.com>
2017-04-19 14:23:46 +0200

    styling

--- public/controller.js
+++ public/controller.js
@@ -107,7 +107,7 @@ LogsFactory.query(
                        legend: { position: 'bottom' },
                        font:20,
                        backgroundColor:'#d7d9d0',
-                       chartArea:{height: 260}
+                       chartArea:{height: 240}
                    };
 
         // Instantiate and draw our chart, passing in some options.
