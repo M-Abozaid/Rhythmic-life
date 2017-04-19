5eae868caf29371db959daf727b2cc067af91d9f
M-Abozaid <aapozaid@gmail.com>
2017-04-19 14:10:25 +0200

    styling

--- public/controller.js
+++ public/controller.js
@@ -105,8 +105,7 @@ LogsFactory.query(
         var options = {'title':'Work',
                        curveType: 'function',
                        legend: { position: 'bottom' },
-                       font:20,
-                       backgroundColor:'#d7d9d0'
+                       font:20
                    };
 
         // Instantiate and draw our chart, passing in some options.
