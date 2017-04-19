d63282e0fefeea80bbba17b9d51c20dcfb75f27d
M-Abozaid <aapozaid@gmail.com>
2017-04-19 14:57:06 +0200

    styling

--- public/controller.js
+++ public/controller.js
@@ -104,7 +104,8 @@ LogsFactory.query(
         // Set chart options
         var options = {'title':'Work',
                        curveType: 'none',
-                       legend: { position: 'top' },
+                       legend: { position: 'top',
+                                alignment:'center' },
                        fontSize:14,
                        backgroundColor:'#d7d9d0',
                        chartArea:{height: 225},
