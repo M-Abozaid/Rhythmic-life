c4888e3cbdac3dd8504b051f2a5e1b0be89764bd
M-Abozaid <aapozaid@gmail.com>
2017-04-20 13:04:48 +0200

    alerts for debugging

--- public/service.js
+++ public/service.js
@@ -1,9 +1,9 @@
 'use strict';
-
+alert('inside service out')
 angular.module('MyBot')
 .constant("baseURL", "https://salty-plains-47076.herokuapp.com/")
 .factory('LogsFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
-		
+		alert('inside service ')
 		let parts = document.URL.split('/')
 		console.log('parts ', parts);
 		let Id = parts[4]
