534da9f2287f5356cbd00c295d52bc51b8e338b5
M-Abozaid <aapozaid@gmail.com>
2017-04-21 10:59:13 +0200

    fix

--- actions/addActivity.js
+++ actions/addActivity.js
@@ -80,6 +80,7 @@ return new Promise(function(resolve, reject){
 										context.current.main = 'getStarted';
 										context.current.chooseLog = true;
 										context.current.continue = true;
+										context.current.first = false;
 										resolve(context)
 									}else{
 										context.current = {}
