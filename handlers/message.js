3d4d708d6b937c2571420d8dc1d6927a4b98832c
M-Abozaid <aapozaid@gmail.com>
2017-04-21 10:20:28 +0200

    debugging

--- handlers/message.js
+++ handlers/message.js
@@ -35,7 +35,7 @@ module.exports = function handleTextMessage (sessionId, session, msg) {
 				}else{
 					session.context = context;
 					sessionStore.saveSession(sessionId, session)
-					//sessionStore.destroy(sessionId)
+					sessionStore.destroy(sessionId)
 				}
 			}
 
