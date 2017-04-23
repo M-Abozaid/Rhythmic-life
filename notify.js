d4cb7fd12c02b8fd425436084ca0a2a2da6cd676
M-Abozaid <aapozaid@gmail.com>
2017-04-23 07:32:09 +0200

    debugging

--- notify.js
+++ notify.js
@@ -14,7 +14,6 @@ module.exports = function(){
 			let sessionId;
 			let session;
 			let newSession;
-			
 			var nowUTC =  Date.now()
 
 			for (var i = users.length - 1; i >= 0; i--) {
@@ -22,43 +21,31 @@ module.exports = function(){
 				let recipientId = user.recipientId
 				let lastActive
 
+
 				if(user.activityLogs.length == 0 ){
-					lastActive = 100
+					let lastLog =  moment.utc(100).add(user.timezone , 'hours')
 				}else {
-					lastActive = user.activityLogs[user.activityLogs.length - 1].createdAt
+					let lastLog =  moment.utc(user.activityLogs[user.activityLogs.length - 1].createdAt).add(user.timezone , 'hours')
 				}
-
 				nowLocal = moment(nowUTC).add(user.timezone , 'hours')
-
-				console.log('moment(lastActive) ',moment(lastActive));
-				lastLog =  moment.utc(lastActive).add(user.timezone , 'hours')
-
-				console.log('lastLog ',lastLog);
 
 
 				 if(nowLocal.hour()>1 && nowLocal.hour()<23 ){
 
-				 	console.log('now hours ',nowLocal.hour());
-				 	console.log(nowLocal.valueOf() ,'  ',lastLog.valueOf());
-				 	if(moment.duration(nowLocal.valueOf() - lastLog.valueOf()).minutes() > 4){  // last active
+				 	if(moment.duration(nowLocal.valueOf() - lastLog.valueOf()).minutes() > 12){  // last active
+				 			
 				 			sessionStore.findOrCreate(recipientId)
 								.then(data => {
-									
 									sessionId = data.sessionId;
 									session = data.session;
 									newSession = data.newSession;
 									let lastNot = session.lastNot || 100;
 									let context = session.context;
-									console.log('lastNot ',lastNot);
-									let lasNotLocal = moment.utc(lastNot).add(user.timezone , 'hours')
-									console.log('lasNotLocal ',lasNotLocal);
-									console.log('nowLocal.valueOf() ',nowLocal.valueOf());
-									console.log('nowLocal.valueOf() ',lasNotLocal.valueOf());
-									let lastNotH = moment.duration(nowLocal.valueOf() - lasNotLocal.valueOf()).minutes()
-									console.log('lastNotH ',lastNotH);
-									//console.log(' vars ','lastNotH ',lastNotH, 'nowLocal.hour() ',nowLocal.hour());
+
+									let lastNotH = moment.duration(nowLocal.valueOf() - moment.utc(lastNot)
+										.add(user.timezone , 'hours').valueOf()).minutes()
+									
 									if(lastNotH > 4 ){
-										console.log('inside last if');
 										let list = _.map(user.activities,(elem)=>{return elem.name})
 										list.push('New activity')
 										let numOfQuick = list.length 
@@ -68,7 +55,6 @@ module.exports = function(){
 											let view = list.splice(context.current.thisVeiw * 10 ,10)
 											view.push("See more!")
 											let data = platformHelpers.generateQuickReplies( user.firstName + '! would you like to add what you\'re doing now', view);
-											console.log('ifffff');
 												GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
 													if(context.current.thisVeiw != numOfVeiws ){context.current.thisVeiw += 1}else{context.current.thisVeiw = 0}
 													context.current = {};
@@ -80,7 +66,6 @@ module.exports = function(){
 												})
 											}else{
 												let data = platformHelpers.generateQuickReplies(user.firstName + '! would you like to add what you\'re doing now', list);
-												console.log(' else');
 												platformHelpers.generateButtonsTemplate('Choose the activity ',[{butn:'option1',},{butn:'opti2'}])
 												GraphAPI.sendTemplateMessage(recipientId, data).then(()=>{
 													context.current = {};
