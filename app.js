a35a10b02d14b14189da35c90b12a5f03487e254
M-Abozaid <aapozaid@gmail.com>
2017-04-25 07:09:45 +0200

    debuggin

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ console.log('port -  ',process.env.PORT);
 
 const server = app.listen(app.get('port'), function () {
     console.log('express server listening on port ' + server.address().port);
-   // notify();
+    notify();
 });
 
 
