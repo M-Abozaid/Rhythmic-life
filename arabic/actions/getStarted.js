917ab0fff8161636b6b0c39c254e9c155db9bc9a
M-Abozaid <aapozaid@gmail.com>
2017-04-26 07:30:15 +0200

    adding validation in getStarted arabic

--- arabic/actions/getStarted.js
+++ arabic/actions/getStarted.js
@@ -2,6 +2,7 @@ const platformHelpers = require('../../platformHelpers');
 const GraphAPI = require('../../graphAPI');
 const mongoose = require('mongoose');
 const User = require('../../schemas/user');
+const _ = require('lodash');
 
 module.exports = function(context, msg){
 // getting started
